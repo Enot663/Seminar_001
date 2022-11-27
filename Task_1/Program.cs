a04bbae2230c41090d397ce3c3deb98dfafd1294
Andrey <andrejsatykov@yandex.ru>
2022-11-27 22:38:05 +0300

    Квадрат числа Task 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+Console.Write("Введите квадрат числа: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+int square = number2*number2; 
+if(square == number1)
+{
+   Console.Write($"Число {number1} является квадратом числа {number2} "); 
+}
+else
+{
+  Console.Write($"Число {number1} не является квадратом числа {number2} ");  
+}
+
+
